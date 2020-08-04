fd58ec1db6c6531599a7de5de86f1ce481f5d084
John Ensley <jensley@nexant.com>
2020-08-04 14:51:37 -0600

    Rewrite

---
+++ census/__init__.py
@@ -0,0 +1,7 @@
+from .api import (
+    query,
+    query_to_df,
+    get_state_fips,
+    get_county_fips,
+    get_variable_list,
+)
--- census/api.py
+++ census/api.py
@@ -1,63 +1,63 @@
 import json
 import os
-from urllib.parse import urlencode, quote
 
 import pandas as pd
-import urllib3
+import requests
 from dotenv import load_dotenv, find_dotenv
 
+load_dotenv(find_dotenv())
+_BASE_URL = 'https://api.census.gov/data/2018/acs/acs5'
 
-def query(http, variables, geography='tract', state=None):
-    if geography == 'zip':
-        geography = 'zip code tabulation area'
-    elif geography != 'tract':
-        raise ValueError('geography must be "tract" or "zip"')
 
-    if state is None:
-        state = '*'
-
-    base_url = 'https://api.census.gov/data/2018/acs/acs5/'
+def construct_params(variables, geography, state='*', county='*'):
     if isinstance(variables, str):
         variables = [variables]
+
+    include_in = True
+    if geography == 'state':
+        state = None
+        county = None
+        include_in = False
+    elif geography == 'county':
+        county = None
+
     params = {
-        'get': f'GEO_ID,NAME,{",".join(variables)}',
-        'for': geography
+        'get': ','.join(variables),
+        'for': f'{geography}:*',
+        'key': os.getenv('CENSUS_API_KEY'),
     }
 
-    if geography == 'tract':
-        params['in'] = f'state:{state}'
+    if include_in:
+        levels = {'state': state, 'county': county}
+        params['in'] = [f'{k}:{v}' for k, v in levels.items() if v is not None]
 
-    params['key'] = os.getenv('CENSUS_API_KEY')
-
-    url = base_url + '?' + urlencode(params, safe='/:,*', quote_via=quote)
-    # print(url)
-    return http.request('GET', url)
+    return params
 
 
-def get_variable_list():
-    base_url = 'https://api.census.gov/data/2018/acs/acs5/'
-    df = pd.read_html(base_url + 'variables.html')[0]
-    df = df.drop('Unnamed: 8', axis=1)
+def query(variables, geography='tract', state='*', county='*'):
+    params = construct_params(variables, geography, state, county)
+    r = requests.get(_BASE_URL, params)
+    r.raise_for_status()
+    return r
+
+
+def query_to_df(response):
+    response_json = json.loads(response.text)
+    df = pd.DataFrame(data=response_json[1:], columns=response_json[0])
     return df
 
 
-class ACSDataset:
-    def __init__(self):
-        load_dotenv(find_dotenv())
-        self.KEY = os.getenv('CENSUS_API_KEY')
-        self.http = urllib3.PoolManager()
-        self.response = None
+def get_state_fips():
+    r = query(['GEO_ID', 'NAME'], 'state')
+    return query_to_df(r).sort_values('NAME')
 
-    def load(self, variables, geography, state=None):
-        self.response = query(self.http, variables, geography, state)
 
-    def to_pandas(self):
-        formatted_response = json.loads(self.response.data.decode('utf-8'))
-        df = pd.DataFrame(columns=formatted_response[0], data=formatted_response[1:])
-        df = df.set_index('GEO_ID')
-        return df
+def get_county_fips(state):
+    r = query(['GEO_ID', 'NAME'], 'county', state=state)
+    return query_to_df(r).sort_values('NAME')
 
-    def to_csv(self, fp, data=None):
-        if data is None:
-            data = self.to_pandas()
-        data.to_csv(fp)
+
+def get_variable_list():
+    df = pd.read_html(_BASE_URL + '/variables.html')[0]
+    df = df.drop('Unnamed: 8', axis=1)
+    return df
